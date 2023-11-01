77470ba5d8263cbf2ae37e80253beabf4f9a838c
Resnu32 <118877654+Resnu32@users.noreply.github.com>
2023-11-01 08:19:43 +0700

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,31 @@
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav ) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// dark
+const darkToggle= document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+    } else {
+        html.classList.remove('dark');
+    }
+});
